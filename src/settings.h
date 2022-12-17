9dbf8b3fbed722f072af5e04bb27c243d78496cf
Joerg Hillebrand <hillebrand@jhillebrand.de>
2022-12-17 13:19:59 +0100

    Switching of a relay implemented

--- src/settings.h
+++ src/settings.h
@@ -2,14 +2,15 @@
 #define DEBUG_MQTT      1 
 #define MODBUS_RATE     9600      // Modbus speed of Growatt, do not change
 #define SERIAL_RATE     115200    // Serial speed for status info
-#define MAX485_DE       5         // D1, DE pin on the TTL to RS485 converter
+#define MAX485_DE       15        // D8, DE pin on the TTL to RS485 converter
 #define MAX485_RE_NEG   4         // D2, RE pin on the TTL to RS485 converter
 #define MAX485_RX       14        // D5, RO pin on the TTL to RS485 converter
 #define MAX485_TX       12        // D6, DI pin on the TTL to RS485 converter
+#define RELAY           3         // D1, switch relay on and off
 #define SLAVE_ID        1         // Default slave ID of Growatt
 #define STATUS_LED      2         // Status LED on the Wemos D1 mini (D4)
-#define UPDATE_MODBUS   60         // 1: modbus device is read every second
-#define UPDATE_STATUS   120        // 10: status mqtt message is sent every 10 seconds
+#define UPDATE_MODBUS   60        // 1: modbus device is read every second
+#define UPDATE_STATUS   95        // 10: status mqtt message is sent every 10 seconds
 #define RGBSTATUSDELAY  500       // delay for turning off the status led
 #define WIFICHECK       500       // how often check lost wifi connection
 
